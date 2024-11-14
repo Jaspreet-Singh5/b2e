401351f90cb900c701c8713a7b27aacdbbb52a58
jaspreetsingh99 <jsingh@zenya.io>
2024-11-14 21:53:03 +0530

    test token contract

---
+++ test/Token.js
@@ -0,0 +1,52 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether');
+}
+
+describe('Token', () => {
+    let token;
+
+    beforeEach(async () => {
+        const Token = await ethers.getContractFactory('Token');
+        token = await Token.deploy(
+            'Bakugo',
+            'BKG',
+            1000000
+        );
+    });
+
+    describe('Deployment', () => {
+        const acutalName = 'Bakugo';
+        const acutalSymbol = 'BKG';
+        const actualDecimals = 18;
+        const actualTotalSupply = tokens(1000000);
+
+        it('has correct name', async () => {
+            const name = await token.name();
+    
+            expect(name).to.equal(acutalName);
+        });
+    
+        it('has correct symbol', async () => {
+            const symbol = await token.symbol();
+    
+            expect(symbol).to.equal(acutalSymbol);
+        });
+    
+        it('has correct decimals', async () => {
+            const decimals = await token.decimals();
+            
+            expect(decimals).to.equal(actualDecimals);
+        });
+    
+        it('has correct total supply', async () => { 
+            const expectedtotalSupply = await token.totalSupply();
+            const acutalTotalSupply = tokens(1000000);
+    
+            expect(expectedtotalSupply.toString()).to.equal(actualTotalSupply.toString());
+        });
+    })
+
+})
