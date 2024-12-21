01c92f74b6eb0b1305877f01b47ece0bc1262b53
jaspreetsingh99 <jsingh@zenya.io>
2024-12-21 19:23:19 +0530

    feat: decorate orders

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,4 +1,29 @@
 import { createSelector } from 'reselect';
+import moment from 'moment';
+const decorateOrder = (order, tokens) => {
+    let token0Amount, token1Amount;
+
+    if (order.tokenGet === tokens[0].address) {
+        token0Amount = order.valueGet;
+        token1Amount = order.valueGive;
+    } else {
+        token0Amount = order.valueGive;
+        token1Amount = order.valueGet;
+    }
+
+    // calc token price to 5 decimal places
+    const precision = 10000;
+    let tokenPrice = token1Amount / token0Amount;
+    tokenPrice = Math.round(tokenPrice * precision) / precision;
+
+    return {
+        ...order,
+        token0Amount,
+        token1Amount,
+        tokenPrice,
+        formattedTimestamp: moment.unix(order.timestamp).format('h:mm:ssa d MMM D'),
+    }
+}
 
 export const orderBookSelector = createSelector([
     state => state.exchange.allOrders.data,
@@ -8,10 +33,11 @@ export const orderBookSelector = createSelector([
     
     // filter by selected trading pair
     const tokenAddresses = new Set([tokens[0].address, tokens[1].address])
-    
-    const filteredOrders = orders.filter(order => (
+
+    orders = orders.filter(order => (
         tokenAddresses.has(order.tokenGet) ||
         tokenAddresses.has(order.tokenGive)
     ));
-    return filteredOrders;
+
+    return orders;
 })
