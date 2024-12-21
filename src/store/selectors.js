a4b3241b7b0f952d93265d3a819484ab9d85d739
jaspreetsingh99 <jsingh@zenya.io>
2024-12-21 19:22:09 +0530

    feat: filter orders

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -4,4 +4,14 @@ export const orderBookSelector = createSelector([
     state => state.exchange.allOrders.data,
     (_, tokens) => tokens
 ], (orders, tokens) => {
+    if (!tokens[0] || !tokens[1]) return;
+    
+    // filter by selected trading pair
+    const tokenAddresses = new Set([tokens[0].address, tokens[1].address])
+    
+    const filteredOrders = orders.filter(order => (
+        tokenAddresses.has(order.tokenGet) ||
+        tokenAddresses.has(order.tokenGive)
+    ));
+    return filteredOrders;
 })
