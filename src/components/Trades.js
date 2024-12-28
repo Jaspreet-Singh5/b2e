9251d99f771f3a7ea7ad676c7c7d1129da517b02
jaspreetsingh99 <jsingh@zenya.io>
2024-12-28 16:06:16 +0530

    feat: display filled orders

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -1,11 +1,14 @@
 import { useSelector } from 'react-redux';
 import { filledOrdersSelector } from '../store/selectors';
 import { useTokensContracts } from '../hooks/useTokensContracts';
+import sort from '../assets/sort.svg';
+import Banner from './Banner';
 
 const Trades = () => {
     const [ tokens ] = useTokensContracts();
 
     const filledOrders = useSelector(state => filledOrdersSelector(state, tokens));
+    const { symbols } = useSelector(state => state.tokens);
 
     return (
         <div className="component exchange__trades">
@@ -13,22 +16,33 @@ const Trades = () => {
                 <h2>Trades</h2>
             </div>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+            {
+                filledOrders?.length > 0
+                ? (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Time<img src={sort} alt='sort' /></th>
+                                <th>{symbols?.[0]}<img src={sort} alt='sort' /></th>
+                                <th>{symbols?.[0]}/{symbols?.[1]}<img src={sort} alt='sort' /></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                filledOrders.map(order => (        
+                                    <tr key={btoa(order.id)}>
+                                        <td>{order.formattedTimestamp}</td>
+                                        <td>{order.token0Amount}</td>
+                                        <td style={{color: order.tokenPriceClass}}>{order.tokenPrice}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                ) : (
+                    <Banner>No Transactions</Banner>
+                )
+            }
 
         </div>
     );
