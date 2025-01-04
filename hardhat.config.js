43960760a76dc88adcd71c544d4bfed1cc46c86f
jaspreetsingh99 <jsingh@zenya.io>
2025-01-04 19:05:08 +0530

    feat: testnet onchain deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,16 @@ require("@nomicfoundation/hardhat-ignition-ethers");
 require("@nomicfoundation/hardhat-ignition");
 require("@nomiclabs/hardhat-ethers");
 
+const privateKeys = process.env.PRIVATE_KEYS || '';
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
   networks: {
-    localhost: {}
+    localhost: {},
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(','),
+    }
   }
 };
