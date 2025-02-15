81aa1837f705546a90bc2eddf359a137af013f51
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-02-15 20:10:16 +0530

    refactor: make token decimals constant

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,7 +6,7 @@ import 'hardhat/console.sol';
 contract Token {
     string public name;
     string public symbol; 
-    uint8 public decimals = 18; 
+    uint8 public constant decimals = 18; 
     uint256 public totalSupply;
 
     // track account balances
