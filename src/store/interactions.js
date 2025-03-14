f09f9c8daa14a0e6153f4cb9e36dcb18c35efdfc
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-03-14 18:11:02 +0530

    refactor: add error handling

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -9,59 +9,70 @@ import axios from "axios";
 export const loadNetwork = async (provider, dispatch) => {
     if (!provider) return;
 
-    const { chainId } = await provider.getNetwork();
-
-    dispatch({
-        type: 'NETWORK_LOADED',
-        chainId
-    });
-
-    return chainId;
+    try {
+        const { chainId } = await provider.getNetwork();
+    
+        dispatch({
+            type: 'NETWORK_LOADED',
+            chainId
+        });
+    
+        return chainId;
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 export const loadAccount = async (dispatch, provider) => {
     if (!provider) return;
 
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const account = ethers.utils.getAddress(accounts[0]);
-
-    dispatch({
-        type: 'ACCOUNT_LOADED',
-        account
-    });
-
-    let balance = await provider.getBalance(account);
-    balance = ethers.utils.formatEther(balance);
-
-    dispatch({
-        type: 'ETHER_BALANCE_LOADED',
-        balance
-    })
-
-    return account;
+    try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = ethers.utils.getAddress(accounts[0]);
+    
+        dispatch({
+            type: 'ACCOUNT_LOADED',
+            account
+        });
+    
+        let balance = await provider.getBalance(account);
+        balance = ethers.utils.formatEther(balance);
+    
+        dispatch({
+            type: 'ETHER_BALANCE_LOADED',
+            balance
+        })
+    
+        return account;
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 export const loadTokens = async (addresses, provider, dispatch) => {
     if (addresses?.filter(Boolean).length !== 2 || !provider) return;
 
-
-    const token1 = new ethers.Contract(addresses[0], TOKEN_ABI, provider);
-    const symbol1 = await token1.symbol();
-
-    dispatch({
-        type: 'TOKEN1_LOADED',
-        symbol: symbol1
-    });
-
-    const token2 = new ethers.Contract(addresses[1], TOKEN_ABI, provider);
-    const symbol2 = await token2.symbol();
-
-    dispatch({
-        type: 'TOKEN2_LOADED',
-        symbol: symbol2
-    });
-
-    return [token1, token2];
+    try {
+        const token1 = new ethers.Contract(addresses[0], TOKEN_ABI, provider);
+        const symbol1 = await token1.symbol();
+    
+        dispatch({
+            type: 'TOKEN1_LOADED',
+            symbol: symbol1
+        });
+    
+        const token2 = new ethers.Contract(addresses[1], TOKEN_ABI, provider);
+        const symbol2 = await token2.symbol();
+    
+        dispatch({
+            type: 'TOKEN2_LOADED',
+            symbol: symbol2
+        });
+    
+        return [token1, token2];
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 // ---------------------------
@@ -69,33 +80,37 @@ export const loadTokens = async (addresses, provider, dispatch) => {
 export const loadBalances = async (tokens, exchange, account, dispatch) => {
     if (tokens?.length !== 2 || !exchange || !account) return;
 
-    let balance = await tokens[0].balanceOf(account);
-    balance = ethers.utils.formatEther(balance);
-    dispatch({
-        type: 'TOKEN1_BALANCE_LOADED',
-        balance
-    });
-
-    balance = await tokens[1].balanceOf(account);
-    balance = ethers.utils.formatEther(balance);
-    dispatch({
-        type: 'TOKEN2_BALANCE_LOADED',
-        balance
-    });
-
-    balance = await exchange.balanceOf(tokens[0].address, account);
-    balance = ethers.utils.formatEther(balance);
-    dispatch({
-        type: 'EXCHANGE_TOKEN1_BALANCE_LOADED',
-        balance
-    });
-
-    balance = await exchange.balanceOf(tokens[1].address, account);
-    balance = ethers.utils.formatEther(balance);
-    dispatch({
-        type: 'EXCHANGE_TOKEN2_BALANCE_LOADED',
-        balance
-    });
+    try {
+        let balance = await tokens[0].balanceOf(account);
+        balance = ethers.utils.formatEther(balance);
+        dispatch({
+            type: 'TOKEN1_BALANCE_LOADED',
+            balance
+        });
+    
+        balance = await tokens[1].balanceOf(account);
+        balance = ethers.utils.formatEther(balance);
+        dispatch({
+            type: 'TOKEN2_BALANCE_LOADED',
+            balance
+        });
+    
+        balance = await exchange.balanceOf(tokens[0].address, account);
+        balance = ethers.utils.formatEther(balance);
+        dispatch({
+            type: 'EXCHANGE_TOKEN1_BALANCE_LOADED',
+            balance
+        });
+    
+        balance = await exchange.balanceOf(tokens[1].address, account);
+        balance = ethers.utils.formatEther(balance);
+        dispatch({
+            type: 'EXCHANGE_TOKEN2_BALANCE_LOADED',
+            balance
+        });
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 // -----------------------------------
@@ -263,32 +278,38 @@ export const orderTokens = async (provider, exchange, orderType, tokens, amount,
 export const loadAllOrders = async (exchange, provider, dispatch) => {
     if (!exchange) return;
 
-    // fetch cancelled orders
-    const cancelledOrdersLogs = await exchange.queryFilter('Cancel');
-    const cancelledOrders = cancelledOrdersLogs.map(cancelledOrdersLog => formatOrder(cancelledOrdersLog.args));
+    try {
 
-    dispatch({
-        type: 'CANCELLED_ORDERS_LOADED',
-        cancelledOrders
-    });
-
-    // fetch filled orders
-    const filledOrdersLogs = await exchange.queryFilter('Trade');
-    const filledOrders = filledOrdersLogs.map(filledOrdersLog => formatOrder(filledOrdersLog.args));
-
-    dispatch({
-        type: 'FILLED_ORDERS_LOADED',
-        filledOrders
-    });
-
-    // fetch all orders
-    const allOrderLogs = await exchange.queryFilter('Order');
-    const allOrders = allOrderLogs.map(orderLog => formatOrder(orderLog.args));
+        // fetch cancelled orders
+        const cancelledOrdersLogs = await exchange.queryFilter('Cancel');
+        const cancelledOrders = cancelledOrdersLogs.map(cancelledOrdersLog => formatOrder(cancelledOrdersLog.args));
     
-    dispatch({
-        type: 'ALL_ORDERS_LOADED',
-        allOrders
-    })
+        dispatch({
+            type: 'CANCELLED_ORDERS_LOADED',
+            cancelledOrders
+        });
+    
+        // fetch filled orders
+        const filledOrdersLogs = await exchange.queryFilter('Trade');
+        const filledOrders = filledOrdersLogs.map(filledOrdersLog => formatOrder(filledOrdersLog.args));
+    
+        dispatch({
+            type: 'FILLED_ORDERS_LOADED',
+            filledOrders
+        });
+    
+        // fetch all orders
+        const allOrderLogs = await exchange.queryFilter('Order');
+        const allOrders = allOrderLogs.map(orderLog => formatOrder(orderLog.args));
+        
+        dispatch({
+            type: 'ALL_ORDERS_LOADED',
+            allOrders
+        })
+    } catch (err) {
+        console.error(err);
+    }
+
 }
 
 export const cancelOrder = async (orderId, exchange, provider, dispatch) => {
