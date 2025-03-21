3788f25140b82cde74e03bc31057cfc9810f6011
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-03-22 00:13:56 +0530

    ui: add styles

--- src/components/OrderBook.js
+++ src/components/OrderBook.js
@@ -7,10 +7,7 @@ import { fillOrder } from '../store/interactions';
 import { useExchangeContract } from '../hooks/useExchangeContract';
 import { useWeb3Connection } from '../hooks/useWeb3Connection';
 import { CircularProgress, styled } from '@mui/material';
-import { 
-    forwardRef,
-    Fragment
-} from 'react';
+import { forwardRef, Fragment } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
@@ -20,42 +17,38 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { TableVirtuoso } from 'react-virtuoso';
 
-const StyledTableCell = styled(TableCell)(({
-    theme
-}) => ({
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
+        backgroundColor: '#2187D0',
+        color: '#fff',
     },
-    [`&.${
-        tableCellClasses.body}`]: {
+    [`&.${tableCellClasses.body}`]: {
         fontSize: 14,
+        color: '#fff',
+    },
+    [`&.${tableCellClasses.head}, &.${tableCellClasses.body}`]: {
+        display: 'table-cell',
     },
 }));
 
-const StyledTableRow = styled(TableRow)(({
-    theme
-}) => ({
-    "&:nth-of-type(odd)": {
-        backgroundColor: theme.palette.action.hover,
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: '#121A29',
     },
     // hide last border
-    "&:last-child td, &:last-child th": {
+    '&:last-child td, &:last-child th': {
         border: 0,
     },
 }));
 
 const OrderBook = () => {
-    const [ tokens ] = useTokensContracts();
+    const [tokens] = useTokensContracts();
     const exchange = useExchangeContract();
     const provider = useWeb3Connection();
-    
+
     const { symbols } = useSelector(state => state.tokens);
     const { data: orderBook, loaded } = useSelector(state => orderBookSelector(state, tokens));
-    const {
-        data: myOpenOrders,
-        loaded: myOpenOrdersLoaded,
-    } = useSelector(state => myOpenOrdersSelector(state, tokens));
+    const { data: myOpenOrders, loaded: myOpenOrdersLoaded } = useSelector(state => myOpenOrdersSelector(state, tokens));
 
     const dispatch = useDispatch();
 
@@ -63,13 +56,15 @@ const OrderBook = () => {
         e.preventDefault();
 
         await fillOrder(orderId, exchange, provider, dispatch);
-    }
+    };
 
-    const checkOwnOrder = (order) => {
+    const checkOwnOrder = order => {
         return myOpenOrders.find(myOpenOrder => myOpenOrder.id === order.id);
-    }
-    
+    };
+
     const createData = (data, symbol0, symbol1) => {
+        if (!Array.isArray(data) || data.length === 0 || !symbol0 || !symbol1) return [];
+
         if (
             !Array.isArray(data) || 
             data.length === 0 ||
@@ -100,7 +95,7 @@ const OrderBook = () => {
             label: symbol1,
             dataKey: symbol1,
         },
-    ];      
+    ];
 
     const fixedHeaderContent = columns => {
         return (
@@ -134,88 +129,72 @@ const OrderBook = () => {
         TableCell: forwardRef((props, ref) => <StyledTableCell {...props} ref={ref} />),
         TableBody: forwardRef((props, ref) => <TableBody {...props} ref={ref} />),
     };
-    
+
     return (
         <div className="component exchange__orderbook">
-            <div className='component__header flex-between'>
+            <div className="component__header flex-between">
                 <h2>Order Book</h2>
             </div>
 
-                {
-                    (loaded && myOpenOrdersLoaded) ? (
-                            <div className="flex items-start">
-                                {
-                                    orderBook?.[OrderType.SELL]
-                                    ? (
-                                        <Paper style={{ height: 400, width: '100%' }}>
-                                            <TableVirtuoso
-                                                data={createData(
-                                                        orderBook[OrderType.SELL],
-                                                        symbols?.[0],
-                                                        symbols?.[1]
-                                                    )
-                                                }
-                                                components={VirtuosoTableComponents}
-                                                fixedHeaderContent={() => fixedHeaderContent(
-                                                    columns(
-                                                        symbols?.[0],
-                                                        symbols?.[1],
-                                                    )
-                                                )}
-                                            itemContent={rowContent}
-                                            />
-                                       </Paper>
-                                    )
-                                    : (
-                                        <p className='flex-center'>No Sell Orders</p>
-                                    )
-                                }
-
-                                <div className='divider'></div>
-                            
-                                {
-                                    orderBook?.[OrderType.BUY]
-                                    ? (
-                                        <table className='exchange__orderbook--buy'>
-                                            <caption>Buying</caption>
-                                            <thead>
-                                                <tr>
-                                                    <th>{symbols?.[0]}<img src={sort} alt='Sort' /></th>
-                                                    <th>{symbols?.[0]}/{symbols?.[1]}<img src={sort} alt='Sort' /></th>
-                                                    <th>{symbols?.[1]}<img src={sort} alt='Sort' /></th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                
-                                                {
-                                                    orderBook[OrderType.BUY].map(order => {
-                                                        return (
-                                                            <tr key={btoa(order.id)}
-                                                                onClick={(e) => fillOrderHandler(e, order.id)}
-                                                            >
-                                                                <td>{order.token0Amount}</td>
-                                                                <td style={{color: order.orderTypeClass}}>{order.tokenPrice}</td>
-                                                                <td>{order.token1Amount}</td>
-                                                            </tr>
-                                                        );
-                                                    })
-                                                }
-                                            </tbody>
-                                        </table>
-                                    )
-                                    : (
-                                        <p className='flex-center'>No Buy Orders</p>
-                                    )
-                                }
-                            </div>
+            {loaded && myOpenOrdersLoaded ? (
+                <div className="flex items-start">
+                    {orderBook?.[OrderType.SELL] ? (
+                        <Paper style={{ height: 400, width: '100%' }}>
+                            <TableVirtuoso
+                                data={createData(orderBook[OrderType.SELL], symbols?.[0], symbols?.[1])}
+                                components={VirtuosoTableComponents}
+                                fixedHeaderContent={() => fixedHeaderContent(columns(symbols?.[0], symbols?.[1]))}
+                                itemContent={rowContent}
+                            />
+                        </Paper>
                     ) : (
-                        <div className="flex justify-center align-center">
-                            <CircularProgress sx={{ color: '#2187D0' }} />
-                        </div>
-                    )
-                }
+                        <p className="flex-center">No Sell Orders</p>
+                    )}
+
+                    <div className="divider"></div>
+
+                    {orderBook?.[OrderType.BUY] ? (
+                        <table className="exchange__orderbook--buy">
+                            <caption>Buying</caption>
+                            <thead>
+                                <tr>
+                                    <th>
+                                        {symbols?.[0]}
+                                        <img src={sort} alt="Sort" />
+                                    </th>
+                                    <th>
+                                        {symbols?.[0]}/{symbols?.[1]}
+                                        <img src={sort} alt="Sort" />
+                                    </th>
+                                    <th>
+                                        {symbols?.[1]}
+                                        <img src={sort} alt="Sort" />
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {orderBook[OrderType.BUY].map(order => {
+                                    return (
+                                        <tr key={btoa(order.id)} onClick={e => fillOrderHandler(e, order.id)}>
+                                            <td>{order.token0Amount}</td>
+                                            <td style={{ color: order.orderTypeClass }}>{order.tokenPrice}</td>
+                                            <td>{order.token1Amount}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    ) : (
+                        <p className="flex-center">No Buy Orders</p>
+                    )}
+                </div>
+            ) : (
+                <div className="flex justify-center align-center">
+                    <CircularProgress sx={{ color: '#2187D0' }} />
+                </div>
+            )}
         </div>
     );
-}
+};
 
 export default OrderBook;
